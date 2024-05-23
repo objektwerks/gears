ed4380cbe50bf66effc3f11b3d9dd1d6d0541801
objektwerks <objektwerks@runbox.com>
2024-05-23 09:15:53 -0400

    added async to joke

--- src/main/scala/objektwerks/Joke.scala
+++ src/main/scala/objektwerks/Joke.scala
@@ -1,9 +1,11 @@
 package objektwerks
 
+import gears.async.Async
+
 import scala.io.{Codec, Source}
 import scala.util.Using
 
-def getJoke(): String =
+def getJoke()(using Async): String =
   Using( Source.fromURL("https://api.chucknorris.io/jokes/random", Codec.UTF8.name) ) { 
     source => s"* ${parseJson( source.mkString )}"
   }.getOrElse("Chuck Norris is taking a power nap! Come back in a few nanoseconds!")
